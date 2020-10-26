5f915290ca535619064d35677edbfce79feeac6a
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-10-26 14:46:37 -0400

    added SearchBar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
     <h1>Pixahunt</h1>
+    <searchbar @search="makeSearch" />
   </div>
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar';
+
 export default {
   name: 'App',
+
+  components: {
+    searchbar: SearchBar,
+  },
+
+  methods: {
+    makeSearch(search) {
+      alert(`searching for ${search}`);
+    },
+  },
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="searchbar">
+    <label for="search">Search</label>
+    <input
+      v-model="search"
+      type="text"
+      id="search"
+      placeholder="Enter pictures to search for..."
+    />
+    <button @click="handleClick">Let's Hunt</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+
+  data() {
+    return {
+      search: '',
+    };
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit('search', this.search);
+      this.search = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.searchbar label {
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+</style>
